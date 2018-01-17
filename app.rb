d4313c16a174687f408f4df79b0b5e9431e19f3f
Meredith Reece <mardebugger@gmail.com>
2018-01-17 18:38:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,10 +19,15 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
 
-  get 'multiply/:num1/:num2' do
-    @num1 = params[:num1].to_i
-    @num2 = params[:num2].to_i
-    "#{@num1*@num2}"
-    resp.status = 200
+  # get 'multiply/:num1/:num2' do
+  #   @num1 = params[:num1].to_i
+  #   @num2 = params[:num2].to_i
+  #   "#{@num1*@num2}"
+  # end
+
+  get "/multiply/:num1/:num2" do
+    @number_1 = params[:num1].to_i
+    @number_2 = params[:num2].to_i
+    "#{@number_1*@number_2}"
   end
 end
